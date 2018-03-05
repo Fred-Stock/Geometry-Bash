f976852ab944397b5dce223bd852d2a484da2acf
cak3042 <cak3042@g.rit.edu>
2018-03-05 10:08:36 -0500

    Added Hud class

---
+++ Geometry_Bash/Geometry_Bash/Hud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry_Bash
+{
+    class Hud
+    {
+        //For Displaying Health bar, Super Meter, Timer, Etc.
+
+    }
+}
