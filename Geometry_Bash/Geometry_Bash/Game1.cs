ab6c83bfa2467a4b8c1fe3f45de7c1350ba49aa6
Kyler McQuillan <yoshi321233@gmail.com>
2018-03-08 21:20:47 -0500

    Added class and base code for map editor

--- Geometry_Bash/Geometry_Bash/Game1.cs
+++ Geometry_Bash/Geometry_Bash/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace Geometry_Bash
 {
---
+++ Geometry_Bash/Geometry_Bash/Map_Editor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Geometry_Bash
+{
+    class Map_Editor
+    {
+        StreamReader reader;
+
+        public Map_Editor()
+        {
+            reader = null;
+
+            try
+            {
+                reader = new StreamReader("");
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
